d9435d52b3bc03e0329f9a0685cefe9483219bf2
Muhammad Lutfi Sultan <52539679+mlutfisultan@users.noreply.github.com>
2021-07-25 06:45:45 +0200

    25072021

--- src/test/java/AndroidAppPackage.java
+++ src/test/java/AndroidAppPackage.java
@@ -1,6 +1,8 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,17 +18,22 @@ public class AndroidAppPackage {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
         caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("platformVersion", "10");
+        caps.setCapability("platformVersion", "11");
         caps.setCapability("deviceName", "Android Emulator");
-        caps.setCapability("appPackage", "");
-        caps.setCapability("appActivity", "");
+        caps.setCapability("appPackage", "com.android.calculator2");
+        caps.setCapability("appActivity", " .Calculator");
 
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
     }
 
     @Test
     public void Click_App_Btn() {
-        driver.findElementByAccessibilityId("App").click();
+        driver.findElement(By.id("digit_5")).click();
+        driver.findElement(By.id("op_add")).click();
+        driver.findElement(By.id("digit_5")).click();
+        driver.findElement(By.id("eq")).click();
+        Assert.assertEquals(driver.findElement(By.id("result")), 10);
+
     }
 
     @AfterTest
