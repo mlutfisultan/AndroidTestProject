5f3509008d77240da60219d1277c9070a7ef824b
Muhammad Lutfi Sultan <52539679+mlutfisultan@users.noreply.github.com>
2021-07-25 05:46:30 +0200

    MuhammadLutfiSultan

---
+++ src/test/java/AndroidAppPackage.java
@@ -0,0 +1,38 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidAppPackage {
+    AppiumDriver driver;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("platformVersion", "10");
+        caps.setCapability("deviceName", "Android Emulator");
+        caps.setCapability("appPackage", "");
+        caps.setCapability("appActivity", "");
+
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+    }
+
+    @Test
+    public void Click_App_Btn() {
+        driver.findElementByAccessibilityId("App").click();
+    }
+
+    @AfterTest
+    public void Tear_Down() {
+        if (null != driver) {
+            driver.quit();
+        }
+    }
+}
