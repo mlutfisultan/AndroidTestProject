a1a1f50e8c077bad2581f01b4a8eafbfa96c6d4e
Muhammad Lutfi Sultan <52539679+mlutfisultan@users.noreply.github.com>
2021-07-30 18:28:44 +0200

    30072021

--- src/test/java/AndroidAppPackage.java
+++ src/test/java/AndroidAppPackage.java
@@ -1,6 +1,8 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
@@ -9,37 +11,55 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class AndroidAppPackage {
     AppiumDriver driver;
+    AppiumDriverLocalService appiumService;
+    String appiumServiceUrl;
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
+
+        appiumService = AppiumDriverLocalService.buildDefaultService();
+        appiumService.start();
+        appiumServiceUrl = appiumService.getUrl().toString();
+        System.out.println("Appium Service Address : - "+ appiumServiceUrl);
+
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
         caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("platformVersion", "11");
+        caps.setCapability("platformVersion", "10");
         caps.setCapability("deviceName", "Android Emulator");
         caps.setCapability("appPackage", "com.android.calculator2");
-        caps.setCapability("appActivity", " .Calculator");
+        caps.setCapability("appActivity", "com.android.calculator2.Calculator");
 
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        driver = new AndroidDriver<>(new URL(appiumServiceUrl), caps);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
     @Test
-    public void Click_App_Btn() {
-        driver.findElement(By.id("digit_5")).click();
-        driver.findElement(By.id("op_add")).click();
-        driver.findElement(By.id("digit_5")).click();
-        driver.findElement(By.id("eq")).click();
-        Assert.assertEquals(driver.findElement(By.id("result")), 10);
+    public void Sum() {
+        System.out.println("Calculate sum of two numbers");
+        // Locate elements to enter data and click +/= buttons
+        driver.findElement(By.xpath("//*[@text='1']")).click();
+        driver.findElement(By.xpath("//*[@text='+']")).click();
+        driver.findElement(By.xpath("//*[@text='2']")).click();
+        driver.findElement(By.xpath("//*[@content-desc='equals']")).click();
 
+        // Get the result text
+        WebElement sumOfNumbersEle = driver.findElement(By.className("android.widget.EditText"));
+        String sumOfNumbers = sumOfNumbersEle.getText();
+
+        // verify if result is 3
+        Assert.assertTrue(sumOfNumbers.endsWith("3"));
     }
 
     @AfterTest
-    public void Tear_Down() {
-        if (null != driver) {
-            driver.quit();
-        }
+    public void End() {
+        System.out.println("Stop driver");
+        driver.quit();
+        System.out.println("Stop appium service");
+        appiumService.stop();
     }
 }
--- src/test/java/FirstAndroidTest.java
+++ src/test/java/FirstAndroidTest.java
@@ -1,5 +1,9 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidTouchAction;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.touch.offset.ElementOption;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -7,30 +11,50 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class FirstAndroidTest {
+    public AndroidTouchAction actions;
     AppiumDriver driver;
+    AppiumDriverLocalService appiumService;
+    String appiumServiceUrl;
+
 
     @BeforeTest
     public void setUp() throws MalformedURLException {
+        appiumService = AppiumDriverLocalService.buildDefaultService();
+        appiumService.start();
+        appiumServiceUrl = appiumService.getUrl().toString();
+        System.out.println("Appium Service Address : - " + appiumServiceUrl);
+
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", "Android");
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("platformVersion", "10");
         caps.setCapability("deviceName", "Android Emulator");
         caps.setCapability("app", System.getProperty("user.dir") + "/Apps/ApiDemos.apk");
-        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+        driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), caps);
+        driver = new AndroidDriver<>(new URL(appiumServiceUrl), caps);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
-    @Test
+    @Test(priority = 1)
     public void Click_App_Btn() {
         driver.findElementByAccessibilityId("App").click();
     }
 
+    @Test(priority = 2)
+    public void Scroll_To_View() {
+        AndroidElement Views_Tab = (AndroidElement) driver.findElementByAccessibilityId("Views");
+        actions = new AndroidTouchAction(driver);
+        actions.tap(ElementOption.element(Views_Tab)).perform();
+    }
+
     @AfterTest
-    public void Tear_Down() {
-        if (null != driver) {
-            driver.quit();
-        }
+    public void End () {
+        System.out.println("Stop driver");
+        driver.quit();
+        System.out.println("Stop appium service");
+        appiumService.stop();
     }
 }
